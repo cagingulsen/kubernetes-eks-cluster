b7ab81dc2040b52f40f64a6419897415c7cbae20
Kemal Cagin Gulsen <cagingulsen@gmail.com>
2019-03-31 12:54:00 +0200

    Update terraform configuration

--- main.tf
+++ main.tf
@@ -7,23 +7,22 @@ terraform {
     encrypt = true
     bucket  = "blueharvest-terraform-state-storage-s3"
     region  = "eu-west-1"
-    key     = "blueharvest/terraform/eks/test51"
+    key     = "blueharvest/terraform/eks/travisTest"
   }
 }
 
 module "blueharvest-eks" {
-  source              = "s3::https://s3-eu-west-1.amazonaws.com/blueharvest-terraform-registry/terraform-aws-blueharvest-eks-v0.0.4-release.zip"
-  // source              = "../terraform-aws-blueharvest-eks"
-  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  eks_ami_id          = "ami-01e08d22b9439c15a" //amazon-eks-node-1.11-v20190109
-  instance_type       = "t2.large"
-  asg_min_size        = "5"
-  asg_max_size        = "20"
-  cluster_name        = "theharvest"
-  cluster_zone        = "blueharvest.io"
-  cluster_zone_id     = "Z31OVNF5EA1VAW"
-  map_users           = []
-  map_roles           = []
-  map_users_count     = 0
-  map_roles_count     = 0
+  source                 = "s3::https://s3-eu-west-1.amazonaws.com/blueharvest-terraform-registry/terraform-aws-blueharvest-eks-v0.0.4-release.zip"
+  availability_zones     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  eks_ami_id             = "ami-01e08d22b9439c15a"
+  instance_type          = "t3.large"
+  asg_min_size           = "5"
+  asg_max_size           = "20"
+  cluster_name           = "travisTest"
+  cluster_zone           = "blueharvest.io"
+  cluster_zone_id        = "Z31OVNF5EA1VAW"
+  letsencrypt_email      = "kemal.gulsen@capgemini.com"
+  letsencrypt_production = "true"
+  map_users              = "${var.map_users}"
+  map_users_count        = "${var.map_users_count}"
 }
